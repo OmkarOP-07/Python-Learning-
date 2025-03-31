74e62c59d9464249fdbb66f785f43d5b40972f5b
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-03-31 18:23:09 +0530

    added 1,2,3py

---
+++ Ass2/3.py
@@ -0,0 +1,7 @@
+cel=float(input("Enter temperature in Celsius"))
+faren = float((cel*(9/5))+32)
+print(cel,"Celsius is ",faren,"Fahrenheit\n")
+
+faren=float(input("Enter temperature in Fahrenheit"))
+cel = (faren-32)*(5/9)
+print(faren,"Fahrenheit is ",cel,"Celsius\n")
