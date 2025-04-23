629b2186cfe9b3f845cb7d17e41cb13dcdf59989
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-04-23 20:06:35 +0530

    strings done

---
+++ Py Harry Cource/chapter3(string)/string_basics.py
@@ -0,0 +1,8 @@
+name = "omkar"
+print(name)
+s1 = name[0:5]
+print(s1)
+print(s1.__len__())
+
+s2 = name[1:5:2]
+print(s2)
---
+++ Py Harry Cource/chapter3(string)/string_functions.py
@@ -0,0 +1,7 @@
+name = "Harmaoni"
+print(len(name))
+print(name.endswith("i"))
+print(name.lower(), name.upper())
+print(name.find("i"))
+print("Hello World \n Omkar \n Heres")
+print('Hello Omkar is a \'name\'') #escape char 
