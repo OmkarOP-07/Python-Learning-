aa2f3c5ffa5403e56bd13a92cd8ed71cd5c85ec0
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-04-23 19:48:37 +0530

    chapter 2 done

---
+++ Py Harry Cource/chapter2/excercise.py
@@ -0,0 +1,8 @@
+a = int(input("enter 1st no :"))
+b = int(input("enter 2nd no :"))
+
+print(f"addition of two no is {(a+b)}")
+print(f"remainder of two no is {(a%b)}")
+print("a is greater than b ", a > b)
+print(f"average of two no is {(a+b)/2}")
+print(f"square of {a} is {(a**2)}")
---
+++ Py Harry Cource/chapter2/variables.py
@@ -0,0 +1,25 @@
+a = 1 
+
+b = "hello"
+
+c = 2.3 
+
+_name = 'omkar' # _ is allowed 
+print(a, b ,c)
+
+a = 1.1 
+b = 2.2 
+c = a+b
+if(a+b == 3.3):
+    print(f"sum is 3.0")
+else:
+    print(f"sum is not {c}")
+    
+# type casting 
+a = "3"
+b = int(a)
+print(type(b))
+ 
+x = input("enter 1st no :")  # it gets ip as string 
+y = input("enter 2nd no :")
+print(x+y)
