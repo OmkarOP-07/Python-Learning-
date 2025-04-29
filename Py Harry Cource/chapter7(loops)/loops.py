f178cf31cd2692ee1cdfbf8f96e49affa3a2a155
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-04-29 20:10:31 +0530

    loops added

---
+++ Py Harry Cource/chapter7(loops)/loops.py
@@ -0,0 +1,35 @@
+# i = 1 
+# while i < 51:
+#     print(i)
+#     i += 1
+    
+# print list using while
+List1 = ["list", 200, 900, "shubham"]
+i = 0
+while(i < len(List1)):
+    print(List1[i])
+    i +=1
+
+#for loops
+# print list
+List1 = ["list",200, "shubham"]
+for i in List1:
+    print(i)
+
+# print tuple
+tuple1 = ("raj", 2, 90.09)
+for i in tuple1:
+    print(i)
+   
+# print string
+string1 = "Harry"
+i = 0
+for i in string1:
+    print(i)
+
+# for loop with else 
+for i in List1:
+    print(i)
+else :
+    print("done")
+    
