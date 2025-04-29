dd8c8654aa022930e98892af68292b426ef2b8d7
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-04-29 19:49:37 +0530

    pr set 6 added

---
+++ Py Harry Cource/chapter6(conditional)/practiceSet.py
@@ -0,0 +1,10 @@
+a = input("Enter first no")
+b = input("Enter second no")
+c = input("Enter third no")
+
+if a > b :
+    print("a is better")
+elif  c > b :
+    print("c is better")
+else :
+    print("b is better")
